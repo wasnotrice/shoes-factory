34cfcd3a08a6a14d73b3394e78259e692a36d853
Eric Watson <wasnotrice@gmail.com>
2012-11-02 18:53:02 -0500

    Add root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Sf::Application.routes.draw do
+  root to: 'root#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
